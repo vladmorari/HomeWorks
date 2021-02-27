60b122ba395d806db59fe606f41716a8895d08f8
vladmorari <morariv67@gmail.com>
2021-02-27 23:01:43 +0200

    26 feb

--- february22.js
+++ february22.js
@@ -1,85 +1,86 @@
-// 1                         Write a JavaScript function that returns the function name.
-//a
-function myFunction() {
-  return arguments.callee.name;
-}
-console.log(`function's name is '${myFunction()}'`);
+// // 1                         Write a JavaScript function that returns the function name.
+// //a
+// function myFunction() {
+//   return arguments.callee.name;
+// }
+// console.log(`function's name is '${myFunction()}'`);
 
-//b
-function functionName(f) {
-  return f.name;
-}
-function functionX() {
-  //some logic
-}
-console.log(functionName(functionX));
+// //b
+// function functionName(f) {
+//   return f.name;
+// }
+// function functionX() {
+//   //some logic
+// }
+// console.log(functionName(functionX));
 
-// 2                       Write a function that receives a random nr of string arguments
-//                         and returns a story generated with the help of these arguments and other custom text.
+// // 2                       Write a function that receives a random nr of string arguments
+// //                         and returns a story generated with the help of these arguments and other custom text.
 
-function randomWords(...words) {
-  return `${
-    words.length != 0
-      ? `
-         Hi,
-         I will create a funny little description about you,
-         depending on how many words I receive from you.
-         So the first word is "${words[0]}" 
-         and I assume this is one of the most important things in your life
-         ${
-           words.length > 1
-             ? `The second word is "${words[1]}"
-         and this word will characterize you as a person ))${
-           words.length > 2
-             ? `
-         I will not talk about the other words 
-         but I will ask you to give them an explanation for each one :
-         ${words}`
-             : `
-         And that's all)`
-         }`
-             : `And that's all)`
-         }`
-      : "There are no parameters"
-  }
-    `;
-}
-console.log(randomWords("work", "happy", "friends"));
+// function randomWords(...words) {
+//   return `${
+//     words.length != 0
+//       ? `
+//          Hi,
+//          I will create a funny little description about you,
+//          depending on how many words I receive from you.
+//          So the first word is "${words[0]}"
+//          and I assume this is one of the most important things in your life
+//          ${
+//            words.length > 1
+//              ? `The second word is "${words[1]}"
+//          and this word will characterize you as a person ))${
+//            words.length > 2
+//              ? `
+//          I will not talk about the other words
+//          but I will ask you to give them an explanation for each one :
+//          ${words}`
+//              : `
+//          And that's all)`
+//          }`
+//              : `And that's all)`
+//          }`
+//       : "There are no parameters"
+//   }
+//     `;
+// }
+// console.log(randomWords("work", "happy", "friends"));
 
-// 3                 Extend the default values functionality (for function parameters) 
-//                    to set to default not only the undefined values but also the null values (but not other falsy values)
+// // 3                 Extend the default values functionality (for function parameters)
+// //                    to set to default not only the undefined values but also the null values (but not other falsy values)
 
-//a
-const myFunc = (a, b) => {
-  let argB;
-  b === null ? (argB = 2) : (argB = b);
-  console.log("a=",a,"b=" , argB);
-};
-myFunc(5, null);
+// //a
+// const myFunc = (a, b) => {
+//   let argB;
+//   b === null ? (argB = 2) : (argB = b);
+//   console.log("a=",a,"b=" , argB);
+// };
+// myFunc(5, null);
 
-//b
-function func(a, b = 2) {
-  b === null ? func(a) : console.log("a=", a, "b=", b);
-}
-func(1, null);
+// //b
+// function func(a, b = 2) {
+//   b === null ? func(a) : console.log("a=", a, "b=", b);
+// }
+// func(1, null);
 
-// 4   Write a function that behaves differently depending on the nr of arguments being passed
+// // 4   Write a function that behaves differently depending on the nr of arguments being passed
 
-function diffBehave(...words) {
-  const areNumbers = words.every(function (element) {
-    return typeof element === "number";
-  });
-  const areStrings = words.every(function (element) {
-    return typeof element === "string";
-  });
-  if (areStrings)
-    return `You offered me ${words.length} strings and i will sort them for you : ${words.sort()} `;
-  if (areNumbers)
-    return `You offered me ${
-      words.length
-    } numbers [${words}] and their sum is : ${words.reduce((a, b) => a + b)} `;
-  else
-    return `You gave me ${words.length} random tdataTypes values and they are : ${words} `;
-}
+// function diffBehave(...words) {
+//   const areNumbers = words.every(function (element) {
+//     return typeof element === "number";
+//   });
+//   const areStrings = words.every(function (element) {
+//     return typeof element === "string";
+//   });
+//   if (areStrings)
+//     return `You offered me ${words.length} strings and i will sort them for you : ${words.sort()} `;
+//   if (areNumbers)
+//     return `You offered me ${
+//       words.length
+//     } numbers [${words}] and their sum is : ${words.reduce((a, b) => a + b)} `;
+//   else
+//     return `You gave me ${words.length} random tdataTypes values and they are : ${words} `;
+// }
 
-console.log(diffBehave("a", "m","w", "v", "a"));
+// console.log(diffBehave("a", "m","w", "v", "a"));
+
---
+++ february26.js
@@ -0,0 +1,76 @@
+// 1             Write a function to check whether input is an array or not without using isArray
+const arr = [2, 5, 8, 10];
+
+const isArray = (e) => {
+  if (e !== null && e !== undefined) return e.constructor === Array;
+  return false;
+};
+
+console.log(isArray(arr));
+
+// 2           Clone an array without using JSON.stringify()
+
+let originArr = [5, 6, 7];
+let cloneArr = [...originArr];
+cloneArr[0] = 1;
+console.log("origin", originArr);
+console.log("clone", cloneArr);
+
+// 3  Create a function that will create an array made of length elements; fill elements with their indexes
+
+const createArr = (len) => {
+  let arr = [];
+  for (let i = 0; i < len; i++) arr.push(i);
+  return arr;
+};
+
+console.log(createArr(8));
+
+//4   Create a function that will return last even element of the array
+let evenElArr = [4, 8, 6, 7, 7, 8, 9, 7];
+const returnLastEven = (arr) => {
+  return arr.reverse().find((el) => el % 2 == 0);
+};
+console.log(returnLastEven(evenElArr));
+
+//5  Write a function that will flatten an array without using flat()
+//  (a) just with numbers
+
+// var myArr = [[1], [[3, [4]], 2], 3, [5]];
+
+// const arrFlatten = (arr) => {
+//   return arr.toString().split(",").map(Number);
+// };
+// console.log(myArr);
+// console.log(arrFlatten(myArr));
+
+// (b) numbers & string
+var myArr = [[1], [[3, ["ab"]], 2], 3, [5]];
+
+const arrFlatten = (arr) => {
+  const arrString = arr.toString().split(",");
+  let newArr = [];
+  arrString.forEach((item) => {
+    if (
+      typeof parseFloat(item) == "number" &&
+      !Number.isNaN(parseFloat(item))
+    ) {
+      newArr.push(parseFloat(item));
+    } else {
+      newArr.push(item);
+    }
+  });
+  return newArr;
+};
+console.log(arrFlatten(myArr));
+
+//6  Write a function to shuffle an array
+
+const arrShuffle = (arr) => {
+  return arr.sort(() => 0.5 - Math.random());
+};
+
+console.log(arrShuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+// if a-b > 0 then he will change their place,
+// in this case a is 0.5 and b sometimes will be less than 0.5 and sometimes not
+//in this case some elements will change their places and other won't
